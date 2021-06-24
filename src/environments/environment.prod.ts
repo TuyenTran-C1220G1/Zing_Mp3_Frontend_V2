cf23ff4633da229ba49150b6fd0dbe499f98395e
TuyenTran-C1220G1 <77034359+TuyenTran-C1220G1@users.noreply.github.com>
2021-06-24 11:01:17 +0700

    done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://zingmp3-backend.herokuapp.com/',
+  firebaseConfig : {
+    apiKey: "AIzaSyCf73DjCou5elcgrko0RnV6UVmAucyIly0",
+    authDomain: "zing-aec15.firebaseapp.com",
+    databaseURL: 'https://zing-aec15-default-rtdb.firebaseio.com',
+    projectId: "zing-aec15",
+    storageBucket: "zing-aec15.appspot.com",
+    messagingSenderId: "945797916148",
+    appId: "1:945797916148:web:87da079f4217fc4373c5a1",
+    measurementId: "G-05YY39DRN9"
+  }
 };
